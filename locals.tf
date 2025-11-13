69241d97f6bb30f10d9aa188951d8f52602af5da
Rajkumar <rajkumarmanda@outlook.com>
2025-11-13 15:21:48 +0530

    terraform

--- locals.tf
+++ locals.tf
@@ -3,7 +3,7 @@ locals {
     ami_id = data.aws_ami.joindevops.id
     vpc_id = data.aws_ssm_parameter.vpc_id.value
     sg_id = data.aws_ssm_parameter.sg_id
-    private_subnet_ids = data.aws_ssm_parameter.private_subnet_ids
+    private_subnet_ids = split("," , data.aws_ssm_parameter.private_subnet_ids.value)
     private_subnet_id = split("," , data.aws_ssm_parameter.private_subnet_ids.value)[0]
     tg_port = "${var.component}" == "frontend" ? 80 : 8080
     health_check_path = "${var.component}" == "frontend" ? "/" : "/health"
